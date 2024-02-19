69a70e3b192ae12a96f0d2d2ca89d603b80cbd64
mana <abdelrahmansameh2299@gmail.com>
2024-02-19 02:17:53 +0200

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,30 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css';
-import LoadingPage from './Components/LoadingPage/LoadingPage';
-import ErrorPage from './Components/ErrorPage/ErrorPage';
-import Popular from './Components/Popular/Popular';
-import TopRated from './Components/TopRated/TopRated';
-import TvShows from './Components/TvShows/TVShows';
-import Layout from './Components/Layout/Layout';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import LoadingPage from "./Components/LoadingPage/LoadingPage";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Popular from "./Components/Popular/Popular";
+import TopRated from "./Components/TopRated/TopRated";
+import TvShows from "./Components/TvShows/TVShows";
+import Layout from "./Components/Layout/Layout";
 
-
-
-
-
-
- const browswrRouter = createBrowserRouter( [ { path:"/", element: <Layout/> , children:[
-   { index : true , element : <Popular/>  },
-   { path:"popular", element : <Popular/>  },
-   { path: "toprated" , element: <TopRated/> },
-   { path : "tvshows" , element : <TvShows/> },
-   { path :"*" , element : <ErrorPage/> }
-   
-  ]  } ]  )
-
-
-
-
-
+const browswrRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Popular /> },
+      { path: "popular", element: <Popular /> },
+      { path: "toprated", element: <TopRated /> },
+      { path: "tvshows", element: <TvShows /> },
+      { path: "*", element: <ErrorPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-
     <>
-
-
-
-    
-    <RouterProvider router={browswrRouter} />
-    
+      <RouterProvider router={browswrRouter} />
     </>
   );
 }
