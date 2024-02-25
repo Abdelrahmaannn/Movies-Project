bf58318dc1352f777734e49b1ed4bd6ec7ea717c
mana <abdelrahmansameh2299@gmail.com>
2024-02-25 14:39:48 +0200

    editing loading page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ function App() {
   return (
     <>
       <RouterProvider router={browswrRouter} />
+
+      {/* <LoadingPage/> */}
     </>
   );
 }
--- src/Components/LoadingPage/LoadingPage.jsx
+++ src/Components/LoadingPage/LoadingPage.jsx
@@ -1,25 +1,29 @@
 import { Component } from "react";
 
-import "./LoadingPage.css"
+import "./LoadingPage.css";
+import { Bars } from "react-loader-spinner";
 
 class LoadingPage extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-            
-            <div className="loading d-flex justify-content-center align-items-center ">
+  state = {};
+  render() {
+    return (
+      <>
+        <div className="loading d-flex justify-content-center align-items-center ">
+          {/* <i class="fa-solid fa-spinner"></i> */}
 
-
-            <i class="fa-solid fa-spinner"></i>
-
-
-            </div>
-            
-            
-            </>
-        );
-    }
+          <Bars
+            height="80"
+            width="80"
+            color="#9B2819"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      </>
+    );
+  }
 }
- 
+
 export default LoadingPage;
